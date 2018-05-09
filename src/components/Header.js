d2837739b5c03d806ff3d7e30a09a5367620ae01
alex <alex.r.hancock@googlemail.com>
2018-05-09 16:15:35 +0100

    ruining header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Header.css";
 
-const Header = ({ currentLocation }) => (
-  <div id="header-box">
-    <h1>
-      Fancy a gig...?{" "}
-      <span role="img" aria-label="rock-on">
-        🤔🤘
-      </span>
-      {currentLocation && <h2>
-        {currentLocation}
-      </h2>}
-    </h1>
-  </div>
-);
+class Header extends Component {
+  state = {
+    currentLocation: null,
+    class: "locName"
+  };
+  shouldComponentUpdate(nextProps) {
+    return nextProps.currentLocation !== this.props.currentLocation;
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.setState({ class: "" });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div id="header-box">
+        <h1>
+          Fancy a gig...?{" "}
+          <span role="img" aria-label="rock-on">
+            🤔🤘
+          </span>
+          {this.props.currentLocation && (
+            <h2 className={this.state.class}>{this.props.currentLocation}</h2>
+          )}
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default Header;
